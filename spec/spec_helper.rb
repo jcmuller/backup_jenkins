b28837c8a71b108af2adbb90fb9de8f019c61abf
Juan C. Müller <jcmuller@gmail.com>
2012-11-06 22:51:31 -0500

    Ignore gems in vendor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ require 'simplecov-rcov'
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start do
   add_filter "spec"
+  add_filter "vendor/bundler_gems"
 end
 
 require 'backup_jenkins'
